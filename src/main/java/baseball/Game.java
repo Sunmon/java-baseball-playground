fd46771d911c85ac39141772c318aa75af0f3040
sunmon <blackmilk274@gmail.com>
2024-08-11 22:15:35 +0900

    fix(controller): generate answer
    숫자게임의 정답 생성 요건 추가
    - 정답 배열의 각 요소는 중복되지 않아야 함

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,9 +4,16 @@ public class Game {
 
     int[] generateAnswer() {
         int[] result = new int[3];
+        boolean isDup = false;
         for(int i = 0; i<result.length; i++) {
             result[i] = (int) ((Math.random() * 9) + 1);
         }
-        return result;
+        // 중복된 숫자 체크
+        for(int i = 0; i<result.length; i++) {
+            isDup = isDup || result[(i+1)%3] == result[i];
+            isDup = isDup || result[(i+2)%3] == result[i];
+        }
+
+        return isDup ? generateAnswer() : result;
     }
 }
