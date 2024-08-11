78a287d88b931df8e39fffc37dd9433f0b932676
sunmon <blackmilk274@gmail.com>
2024-08-11 22:16:54 +0900

    test(controller): check answer
    숫자게임 정답 체크 테스트 추가
    - 값만 맞으면 ball
    - 값과 자리가 맞으면 strike

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -3,7 +3,13 @@ package baseball;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,4 +36,24 @@ class GameTest {
 
         assertThat(result).containsOnlyOnce(result[0], result[1], result[2]);
     }
+
+
+    static Iterable<Arguments> getTestData() {
+        return List.of(
+                Arguments.of(new int[]{1, 2, 3},new int[]{3,2,1},new int[] {2,1}),
+                Arguments.of(new int[]{4,2,3},new int[]{4,2,3},new int[] {0,3}),
+                Arguments.of(new int[]{6,5,4},new int[]{5,4,6},new int[]{3,0}),
+                Arguments.of(new int[]{1,2,3},new int[]{4,5,6},new int[]{0,0})
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("스트라이크와 볼 개수를 판단한다")
+    @MethodSource("getTestData")
+    void checkAnswer(int[] answer, int[] userInput, int[] expected) {
+        int[] result = game.checkAnswer(answer, userInput);
+        assertThat(result).isEqualTo(expected);
+    }
+
+
 }
