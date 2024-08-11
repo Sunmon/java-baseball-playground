f2ab46e107eefaf35bc800df6d21474d38bfee35
sunmon <blackmilk274@gmail.com>
2024-08-11 23:56:29 +0900

    test: fix test setup
    Game, Input, Output View 변경에 따른 테스트 셋업 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -21,7 +21,8 @@ class GameTest {
 
     @BeforeAll
     static void beforeAll() {
-        game = new Game();
+        InputView input = new InputView();
+        game = new Game(input);
     }
 
     @Test
