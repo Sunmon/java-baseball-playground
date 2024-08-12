95606049b6e44e7bb3c95e691ee3a5ca226357d0
sunmon <blackmilk274@gmail.com>
2024-08-12 22:29:13 +0900

    feat(Ball): Compare ball value
    공끼리 값이 같은지 비교하는 함수 추가

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,7 +1,21 @@
 package baseball;
 
 public class Ball {
+    private final int value;
+
+    public Ball() {
+        this.value = getRandNum();
+    }
+
+    public Ball(int value) {
+        this.value = value;
+    }
+
     public int getRandNum() {
         return (int)(Math.random()*9) + 1;
+    }
+
+    public boolean isEqualTo(Ball user) {
+        return user.value == this.value;
     }
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -14,4 +14,14 @@ public class BallTest {
         assertThat(num).isBetween(1,9);
     }
 
+    @Test
+    @DisplayName("두 공이 같은 값을 가지는지 비교한다")
+    void isEqualTest() {
+        Ball ball1 = new Ball(1);
+        Ball ball2 = new Ball(2);
+        Ball ball3 = new Ball(1);
+        assertThat(ball1.isEqualTo(ball2)).isFalse();
+        assertThat(ball1.isEqualTo(ball3)).isTrue();
+    }
+
 }
